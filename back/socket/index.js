a82428245b69d060db790bfd94e6348d67795258
Potion Story <potionstory@gmail.com>
2018-06-09 00:19:23 +0900

    socket setting

--- back/socket/index.js
+++ back/socket/index.js
@@ -4,26 +4,25 @@ let user = []; // 사용자 리스트
 
 const handleErrorSocket = (ws) => {
   ws.on('close', () => { console.log('disconnected'); });
-  we.on('error', () => { console.log('error'); });
+  ws.on('error', () => { console.log('error'); });
 }
 
 // 메시지 전송
 const sendMessage = (ws, message) => ws.send(message);
 
 // 모든 사용자에게 메시지 전송
-const sendAllUser = (users, message) => users.forEach(we => sendMessage(we, message));
+const sendAllUser = (users, message) => users.forEach(ws => sendMessage(ws, message));
 
 const startWebSocket = server => {
   const wsServer = new WebSocket.Server({ server });
 
   // 접속시 아래 내용이 실행
-  wsServer.on('connnection', (ws, req) => {
+  wsServer.on('connection', (ws, req) => {
     handleErrorSocket(ws); // Error 처리 연동
 
     // message가 발생하면 처리
     ws.on('message', message => {
       try {
-        console.log(message);
         const data = JSON.parse(message); // 전송은 Text 형식으로만 가능
         sendAllUser(wsServer.clients, data.message); // 받은 데이터를 모든 사용자에게 전송
       } catch(error) {
